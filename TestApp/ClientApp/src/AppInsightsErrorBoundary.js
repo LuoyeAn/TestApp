f691bc63710368b4f75dc684553d428cdf4943d0
Tingting An <TingtingAn@highviewsoftware.com>
2022-11-16 15:48:15 +0800

    没有触发error boundary

---
+++ TestApp/ClientApp/src/AppInsightsErrorBoundary.js
@@ -0,0 +1,28 @@
+﻿import React from "react";
+import { SeverityLevel } from "@microsoft/applicationinsights-web";
+
+export default class AppInsightsErrorBoundary extends React.Component {
+    state = { hasError: false };
+
+    componentDidCatch(error, info) {
+        console.log("eror output");
+        this.setState({ hasError: true });
+        //this.props.appInsights.trackException({
+        //    error: error,
+        //    exception: error,
+        //    severityLevel: SeverityLevel.Error,
+        //    properties: { ...info }
+        //});
+    }
+
+    render() {
+        if (this.state.hasError) {
+            const { onError } = this.props;
+            return typeof onError === "function"
+                ? onError()
+                : React.createElement(onError);
+        }
+
+        return this.props.children;
+    }
+}
